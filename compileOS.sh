682154e2ec2de4b1df9e23a7e6939abda1d188ad
nfgallimore <nfgallimore@gmail.com>
2019-02-01 00:21:47 -0500

    Update utility scripts.

--- compileOS.sh
+++ compileOS.sh
@@ -1,6 +1,9 @@
 # Compiles the BlackDOS operating system
 # Author: Nick Gallimore
 
+# Compile bootloader assembly
+nasm bootload.asm
+
 # Creates blank disk image
 dd if=/dev/zero of=floppya.img bs=512 count=2880
 
@@ -10,7 +13,10 @@ dd if=bootload of=floppya.img bs=512 count=1 conv=notrunc
 # Compiles kernel.c
 bcc -ansi -c -o kernel.o kernel.c
 
-# Links kernel.o with kernel_asm.o
+# Compile kernel assembly
+as86 kernel.asm -o kasm.o
+
+# Links kernel.o with kasm.o
 ld86 -o kernel -d kernel.o kasm.o
 
 # Puts kernel into sector 259
